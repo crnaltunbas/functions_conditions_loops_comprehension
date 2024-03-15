40cef5d8b522f2228d43a85f1b2b7754d6b721bb
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-03-16 02:39:09 +0300

    Return fonksiyonu anlatıldı

--- function_contions_loops_comprehensions.py
+++ function_contions_loops_comprehensions.py
@@ -132,3 +132,39 @@ def calculate(varm, moisture, charge):
 calculate(98, 12, 78)
 
 
+#  Return:Fonksiyon çıktılarını girdi olarak kullanmak
+
+def calculate(varm, moisture, charge):
+  print((varm + moisture)/charge)
+
+#calculate(98, 12, 78) * 10
+
+def calculate(varm, moisture, charge):
+   return((varm + moisture)/charge)
+
+calculate(98, 12, 78) * 10
+a = calculate(98, 12,78)
+
+
+def calculate(varm, moisture, charge):
+    varm = varm * 2
+    moisture = moisture * 2
+    charge = charge * 2
+    output = (varm + moisture) / charge
+
+    return varm, moisture, charge, output
+
+calculate(98,12, 78)
+
+type(calculate(98,12, 78))
+
+varm, moisture, charge, output = calculate(98, 12, 78)
+
+calculate(98, 12, 78) * 10
+
+
+
+
+
+
+
