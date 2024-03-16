faa132993651e43de6890658b12ce8dfffddcc6b
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-03-16 21:40:12 +0300

    alternating fonksiyonunun enumerate ile yazılması

--- function_contions_loops_comprehensions.py
+++ function_contions_loops_comprehensions.py
@@ -431,6 +431,16 @@ def divide_students(students):
 divide_students(students)
 
 
+#  alternating fonksiyonun enumerate ile yazılması
 
+def alternating_with_enumerate(string):
+    new_string = ""
+    for i, letter in enumerate(string):
+      if i % 2 == 0:
+        new_string += letter.upper()
+      else:
+        new_string += letter.lower()
+    print(new_string)
+alternating_with_enumerate("hi my name is john and i am learning python")
 
 
