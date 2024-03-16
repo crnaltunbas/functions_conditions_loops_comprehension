492d8b18382464e26a4f5c1d30fcb2a6df860afb
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-03-16 21:32:07 +0300

    Enumerate Uygulama, mülakat sorusu çözüldü.

--- function_contions_loops_comprehensions.py
+++ function_contions_loops_comprehensions.py
@@ -409,6 +409,27 @@ print(B)
 
 
 
+# Uygulama - Mülakat sorusu
+# divide_ students fonksiyonu yazınız.
+# Çift indexte yer alan öğrencileri bir listeye alınız.
+# Tek indexte yer alan öğrencileri bir başka listeye alınız.
+# Fakat bu iki liste tek bir liste olarak return olsun.
+
+students = ["John", "Mark", "Vanessa", "Mariam"]
+
+
+def divide_students(students):
+  groups = [[], []]
+  for index, student in enumerate( students):
+    if index % 2 == 0 :
+      groups[0].append(student)
+    else:
+      groups[1].append(student)
+  print(groups)
+  return
+
+divide_students(students)
+
 
 
 
